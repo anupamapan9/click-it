51239a6a8d40f49af5cac1787b28319edfddc22d
anupamapan9 <anupamapan0905@gmail.com>
2022-04-17 02:10:46 +0600

    register added

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import { Route, Routes } from 'react-router-dom';
 import './App.css';
 import Banner from './Pages/Home/Banner/Banner';
 import LogIn from './Pages/LogIn/LogIn';
+import Register from './Pages/Register/Register';
 import Header from './Sheared/Header/Header'
 function App() {
   return (
@@ -9,6 +10,7 @@ function App() {
       <Header></Header>
       <Routes>
         <Route path='/log-in' element={<LogIn></LogIn>}></Route>
+        <Route path='/register' element={<Register></Register>}></Route>
       </Routes>
     </div>
   );
--- src/Pages/Register/Register.js
+++ src/Pages/Register/Register.js
@@ -6,20 +6,26 @@ const Register = () => {
         <section className='flex h-[80vh] justify-center items-center'>
             <div className='md:w-[400px]  border-2 shadow-lg shadow-red-500 px-14 py-9'>
                 <h1 className='font-extrabold text-2xl'>
-                    Login
+                    Register
                 </h1>
                 <form>
+                    <div className='my-10'>
+                        <input type="text" name="name" id="" placeholder='Enter Your Name' className='border-b-2 border-[#fb5050] transition-all w-full focus:outline-none focus:border-[#ff0000]' required />
+                    </div>
                     <div className='my-10'>
                         <input type="email" name="email" id="" placeholder='Enter Your Email' className='border-b-2 border-[#fb5050] transition-all w-full focus:outline-none focus:border-[#ff0000]' required />
                     </div>
                     <div className='my-10'>
                         <input type="password" name="password" id="" placeholder='Enter Your Password' className='border-b-2 border-[#fb5050] focus:outline-none w-full transition-all focus:border-[#ff0000]' required />
                     </div>
+                    <div className='my-10'>
+                        <input type="password" name="confirm password" id="" placeholder='Enter Your Password Again' className='border-b-2 border-[#fb5050] focus:outline-none w-full transition-all focus:border-[#ff0000]' required />
+                    </div>
                     <div className='my-5'>
-                        <input type="submit" value="Login" className='bg-[#fb5050] transition-all hover:bg-[#ff0000]  w-full py-2 cursor-pointer  ' />
+                        <input type="submit" value="Register" className='bg-[#fb5050] transition-all hover:bg-[#ff0000]  w-full py-2 cursor-pointer  ' />
                     </div>
                 </form>
-                <p>New Here? <Link className='border-b-2 border-[#fb5050] hover:text-[#ff0000]' to='/register'>Register Now</Link></p>
+                <p>Have an Account? <Link className='border-b-2 border-[#fb5050] hover:text-[#ff0000]' to='/log-in'>Login</Link></p>
             </div>
 
         </section>
